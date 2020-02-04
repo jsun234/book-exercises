44844e9f7fa12ca90e7b5c241e8c14bcd367dcff
Jenny Sun <jennysun@uw.edu>
2020-02-04 14:53:24 -0800

    Work on exercise 5, chapter 11

--- chapter-11-exercises/exercise-5/exercise.R
+++ chapter-11-exercises/exercise-5/exercise.R
@@ -1,18 +1,32 @@
-# Exercise 5: dplyr grouped operations
+# Chapter 11 Exercise 5: dplyr grouped operations
 
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
 #install.packages("nycflights13")  # should be done already
 library("nycflights13")
 library("dplyr")
+View(flights)
 
 # What was the average departure delay in each month?
 # Save this as a data frame `dep_delay_by_month`
 # Hint: you'll have to perform a grouping operation then summarizing your data
-
+dep_delay_by_month <- 
+  flights %>%
+  group_by(month) %>% 
+  summarize(mean(dep_delay)) %>% 
+  
+  
+  
 
 # Which month had the greatest average departure delay?
-
+delay_by_month <- 
+  flights %>% 
+  group_by(month) %>% #creates 12 differents month data frames
+  summarize(
+    avg_dep_delay = mean(dep_delay, na.rm = TRUE)
+  )# %>% 
+ # filter (avg_dep_delay == max(avg_dep_delay))
+plot(delay_by_month)
 
 # If your above data frame contains just two columns (e.g., "month", and "delay"
 # in that order), you can create a scatterplot by passing that data frame to the
@@ -22,10 +36,28 @@ library("dplyr")
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation then summarize your data
 # You can use the `head()` function to view just the first few rows
-
+flights %>% 
+  group_by(dest) %>% 
+  summarize(
+    avg_arr_delay = mean(arr_delay, na.rm = TRUE)
+  ) %>% 
+  filter(avg_arr_delay == max(avg_arr_delay, na.rm = TRUE))
 
 # You can look up these airports in the `airports` data frame!
-
-
+View(airports)
+airports %>% 
+  filter(faa == "CAE")
 # Which city was flown to with the highest average speed?
-
+flights %>% 
+  group_by(dest) %>% #consirdering things by dest for the city
+  summarize(
+    avg_speed = mean(distance/air_time, na.rm = TRUE) #speed = d/t
+  ) %>% 
+  filter(avg_speed == max(avg_speed, na.rm = TRUE))
+airports %>% 
+  filter(faa == "ANC")
+# airports are in a seperate dataframe because 
+# 1. This would get repeated
+# 2. This is not relevant to the flight
+airports %>% 
+  filter(dest == "ANC")
