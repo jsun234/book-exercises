afe688f0bc3d4c0d9246c07c2a6a86cbd0adce3d
Jenny Sun <jennysun@uw.edu>
2020-02-28 17:59:08 -0800

    Chapter 19 practice

--- chapter-19-exercises/exercise-4/app.R
+++ chapter-19-exercises/exercise-4/app.R
@@ -1,3 +1,4 @@
+# Chapter 19
 # Exercise 4: complex Shiny UI layouts
 
 # Load libraries so they are available
@@ -5,8 +6,10 @@ library("shiny")
 
 # Use source() to execute the `app_ui.R` and `app_server.R` files. These will
 # define the UI value and server function respectively.
-
+source("app_ui.R") # sourcing different r files based on class
+source("app_server.R") 
 # You will need to fill in the `app_ui.R` file to create the layout.
 # Run the app through this file.
 
 # Create a new `shinyApp()` using the loaded `ui` and `server` variables
+shinyApp(ui = ui, server = server)
